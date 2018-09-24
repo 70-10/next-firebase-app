4ccc43477bb6df1cd15081cad765a983f05ac7e1
70_10 <7010.dev@gmail.com>
2018-09-24 12:59:45 +0900

    [mod] bump update dependencies

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-const withProgressBar = require("next-progressbar");
 const withSass = require("@zeit/next-sass");
 
-module.exports = withProgressBar(
-  withSass({
-    exportPathMap: defaultPathMap => ({
-      "/": { page: "/" },
-      "/about": { page: "/about" },
-    }),
-  })
-);
+module.exports = withSass({
+  exportPathMap: defaultPathMap => ({
+    "/": { page: "/" },
+    "/about": { page: "/about" },
+  }),
+});
